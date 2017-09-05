a597659ea3f18f5f5d18ab634b9b42865faf72ba
zctao <zt75@cornell.edu>
2017-09-05 16:30:24 -0500

    Scripts to print event list of ntuple

---
+++ ttHtautauAnalyzer/scripts/printEventList.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+inname=$1
+outname=${2:-eventList.txt}
+treename=${3:-eventTree}
+directory=${4:-ttHtaus}
+
+root -b $inname <<EOF
+$directory->cd();
+$treename->SetScanField(0);
+.> $outname
+$treename->Scan("run:ls:nEvent");
+.>
+EOF
