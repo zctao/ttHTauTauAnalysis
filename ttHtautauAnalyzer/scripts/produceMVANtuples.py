30a196ea2458d7c0c1513d3e169d49b1f0ab05a5
zctao <zt75@cornell.edu>
2018-01-15 16:06:16 -0600

    python script to make MVA Ntuples

---
+++ ttHtautauAnalyzer/scripts/produceMVANtuples.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+import ttHTauTauAnalysis.ttHtautauAnalyzer.datacards as dc
+
+parser = argparse.ArgumentParser(description='produce MVA ntuples using makeMVANtuple.cc')
+
+parser.add_argument('anatype', choices=['1l2tau','2lss1tau','3l1tau'],
+                    help='analysis type')
+parser.add_argument('samples', nargs='+',
+                    choices=['ttH','TTW','TTZ','TTGJets','TGJets','WG','ZG','WZ','ZZ','WW','WWds','WpWp','WZZ','WWZ','WWW','ZZZ','tZq','TTTT','fakes_data','flips_data','data_obs'],
+                    help="sample names")
+parser.add_argument('-d','--datasets', nargs='+', choices=['SingleMuon','SingleElectron','DoubleMuon','DoubleEG','MuonEG'],
+                    default=['SingleMuon','SingleElectron','DoubleMuon','DoubleEG','MuonEG'], help="dataset for collision data sample")
+parser.add_argument('-c','--corrections', nargs='+',
+                    choices=['jesup','jesdown','tesup','tesdown'], default=None,
+                    help="Jet/Tau energy correction")
+parser.add_argument('-s','--doSystematics', action='store_true',
+                    help="include event weights for systematic uncertainties")
+parser.add_argument('-o','--outdir', type=str, default='./', help="Output directory")
+parser.add_argument('-l','--list', type=str, default='ntuplelist.log',
+                    help="list of event ntuple to be processed")
+parser.add_argument('-r','--redirector', type=str,
+                    default='root://cmsxrootd.fnal.gov/',help="redirector for xrootd")
+
+args = parser.parse_args()
+
+mvantupleList = open(args.outdir+'mvaNtuples.txt','w')
+
+dosys_str = 'true' if args.doSystematics else 'false'
+
+for sample in args.samples:
+
+    # determine seltype
+    seltype = 'signal_'+args.anatype
+    if 'fakes_data' in sample:
+        seltype = 'control_fake_'+args.anatype
+    elif 'flips_data' in sample:
+        assert(anatype=='2lss1tau')
+        seltype = 'control_2los1tau'
+    
+    if 'data' in sample:
+        assert(len(args.datasets)>0)
+        for dataset in args.datasets:
+            fname = dc.getNtupleFileName_data(args.list, args.anatype, sample, dataset)
+            if fname is None:
+                continue
+            
+            ntuplename = args.redirector+fname
+            outputname = args.outdir+'mvaNtuple_'+sample+'_'+dataset+'_'+args.anatype+'.root'
+            
+            os.system('makeMVANtuple -i '+ntuplename+' -o '+outputname+' --anatype '+args.anatype+' --seltype '+seltype+' -d true -m false -e true -u true -w true -s '+dosys_str)
+            mvantupleList.write(outputname+'\n')
+    else:
+        fname = dc.getNtupleFileName_mc(args.list, args.anatype, sample)
+        if fname is None:
+            continue
+        
+        ntuplename = args.redirector+fname
+        outputname = args.outdir+'mvaNtuple_'+sample+'_'+args.anatype+'.root'
+        os.system('makeMVANtuple -i '+ntuplename+' -o '+outputname+' --anatype '+args.anatype+' --seltype '+seltype+' -e true -u true -w true -s '+dosys_str)
+        mvantupleList.write(outputname+'\n')
+
+        if args.doSystematics:
+            for cor in args.corrections:
+                fname_cor = dc.getNtupleFileName_mc(args.list, args.anatype, sample, correction=cor)
+                if fname_cor is None:
+                    continue
+                
+                ntuplename = args.redirector+fname_cor
+                outputname = args.outdir+'mvaNtuple_'+sample+'_'+cor+'_'+args.anatype+'.root'
+                print 'here'
+                print outputname
+                
+                os.system('makeMVANtuple -i '+ntuplename+' -o '+outputname+' --anatype '+args.anatype+' --seltype '+seltype+' -e true -u true -w true -s '+dosys_str)
+                mvantupleList.write(outputname+'\n')
--- ttHtautauAnalyzer/test/produceMVANtuplesFastSim16.sh
+++ ttHtautauAnalyzer/test/produceMVANtuplesFastSim16.sh
@@ -11,36 +11,36 @@ tthfn=$(python -c "import sys; from ttHTauTauAnalysis.ttHtautauAnalyzer.crab_uti
 ttHntuple_1l2tau=$redirector$eostopdir$tthfn'/'$era'/ntuple_ttH_1l2tau_loose.root'
 ttHntuple_2lss1tau=$redirector$eostopdir$tthfn'/'$era'/ntuple_ttH_2lss1tau_loose.root'
 
-makeMVANtuple -i $ttHntuple_1l2tau -o $outdir --sample ttH --anatype 1l2tau --seltype loose_1l2tau --systematics false --loose_selection true
-makeMVANtuple -i $ttHntuple_2lss1tau -o $outdir --sample ttH --anatype 2lss1tau --seltype loose_2lss1tau --systematics false --loose_selection true
+makeMVANtuple -i $ttHntuple_1l2tau -o $outdir'mvaVars_ttH_1l2tau.root' --anatype 1l2tau --seltype loose_1l2tau --systematics false --loose_selection true
+makeMVANtuple -i $ttHntuple_2lss1tau -o $outdir'mvaVars_ttH_2lss1tau.root' --anatype 2lss1tau --seltype loose_2lss1tau --systematics false --loose_selection true
 
 echo 'TT'
 ttdlfn=$(python -c "import sys; from ttHTauTauAnalysis.ttHtautauAnalyzer.crab_utils import getSampleFullname; print getSampleFullname(sys.argv[1],'$samplelist')" TT_DiLep_p1)
 TTdlntuple_1l2tau=$redirector$eostopdir$ttdlfn'/'$era'/ntuple_TT_DiLep_1l2tau_loose.root'
 TTdlntuple_2lss1tau=$redirector$eostopdir$ttdlfn'/'$era'/ntuple_TT_DiLep_2lss1tau_loose.root'
 
-makeMVANtuple -i $TTdlntuple_1l2tau -o $outdir --sample TT_DiLep --anatype 1l2tau --seltype loose_1l2tau --systematics false --loose_selection true --mc_matching false
-makeMVANtuple -i $TTdlntuple_2lss1tau -o $outdir --sample TT_DiLep --anatype 2lss1tau --seltype loose_2lss1tau --systematics false --loose_selection true
+makeMVANtuple -i $TTdlntuple_1l2tau -o $outdir'mvaVars_TT_DiLep_1l2tau.root' --anatype 1l2tau --seltype loose_1l2tau --systematics false --loose_selection true --mc_matching false
+makeMVANtuple -i $TTdlntuple_2lss1tau -o $outdir'mvaVars_TT_DiLep_2lss1tau.root' --anatype 2lss1tau --seltype loose_2lss1tau --systematics false --loose_selection true
 
 ttslfn=$(python -c "import sys; from ttHTauTauAnalysis.ttHtautauAnalyzer.crab_utils import getSampleFullname; print getSampleFullname(sys.argv[1],'$samplelist')" TT_SemiLep_p1)
 TTslntuple_1l2tau=$redirector$eostopdir$ttslfn'/'$era'/ntuple_TT_SemiLep_1l2tau_loose.root'
 TTslntuple_2lss1tau=$redirector$eostopdir$ttslfn'/'$era'/ntuple_TT_SemiLep_2lss1tau_loose.root'
 
-makeMVANtuple -i $TTslntuple_1l2tau -o $outdir --sample TT_SemiLep --anatype 1l2tau --seltype loose_1l2tau --systematics false --loose_selection true --mc_matching false
-makeMVANtuple -i $TTslntuple_2lss1tau -o $outdir --sample TT_SemiLep --anatype 2lss1tau --seltype loose_2lss1tau --systematics false --loose_selection true
+makeMVANtuple -i $TTslntuple_1l2tau -o $outdir'mvaVars_TT_SemiLep_1l2tau.root' --anatype 1l2tau --seltype loose_1l2tau --systematics false --loose_selection true --mc_matching false
+makeMVANtuple -i $TTslntuple_2lss1tau -o $outdir'mvaVars_TT_SemiLep_2lss1tau.root' --anatype 2lss1tau --seltype loose_2lss1tau --systematics false --loose_selection true
 
 echo 'TTW'
 ttwfn=$(python -c "import sys; from ttHTauTauAnalysis.ttHtautauAnalyzer.crab_utils import getSampleFullname; print getSampleFullname(sys.argv[1],'$samplelist')" TTW)
 ttwntuple_1l2tau=$redirector$eostopdir$ttwfn'/'$era'/ntuple_TTW_1l2tau_loose.root'
 ttwntuple_2lss1tau=$redirector$eostopdir$ttwfn'/'$era'/ntuple_TTW_2lss1tau_loose.root'
 
-makeMVANtuple -i $ttwntuple_1l2tau -o $outdir --sample TTW --anatype 1l2tau --seltype loose_1l2tau --systematics false --loose_selection true
-makeMVANtuple -i $ttwntuple_2lss1tau -o $outdir --sample TTW --anatype 2lss1tau --seltype loose_2lss1tau --systematics false --loose_selection true
+makeMVANtuple -i $ttwntuple_1l2tau -o $outdir'mvaVars_TTW_1l2tau.root' --anatype 1l2tau --seltype loose_1l2tau --systematics false --loose_selection true
+makeMVANtuple -i $ttwntuple_2lss1tau -o $outdir'mvaVars_TTW_2lss1tau.root' --anatype 2lss1tau --seltype loose_2lss1tau --systematics false --loose_selection true
 
 echo 'TTZ'
 ttzfn=$(python -c "import sys; from ttHTauTauAnalysis.ttHtautauAnalyzer.crab_utils import getSampleFullname; print getSampleFullname(sys.argv[1],'$samplelist')" TTZ)
 ttzntuple_1l2tau=$redirector$eostopdir$ttzfn'/'$era'/ntuple_TTZ_1l2tau_loose.root'
 ttzntuple_2lss1tau=$redirector$eostopdir$ttzfn'/'$era'/ntuple_TTZ_2lss1tau_loose.root'
 
-makeMVANtuple -i $ttzntuple_1l2tau -o $outdir --sample TTZ --anatype 1l2tau --seltype loose_1l2tau --systematics false --loose_selection true
-makeMVANtuple -i $ttzntuple_2lss1tau -o $outdir --sample TTZ --anatype 2lss1tau --seltype loose_2lss1tau --systematics false --loose_selection true
+makeMVANtuple -i $ttzntuple_1l2tau -o $outdir'mvaVars_TTZ_1l2tau.root' --anatype 1l2tau --seltype loose_1l2tau --systematics false --loose_selection true
+makeMVANtuple -i $ttzntuple_2lss1tau -o $outdir'mvaVars_TTZ_2lss1tau.root' --anatype 2lss1tau --seltype loose_2lss1tau --systematics false --loose_selection true
