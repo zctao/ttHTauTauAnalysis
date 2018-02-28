0a660d82e18506b8880718ab49a3ed57d52dce05
zctao <zt75@cornell.edu>
2018-02-28 15:09:54 -0600

    Update crab submitting scripts

--- ttHtautauAnalyzer/test/submitCrabFastSim2016.sh
+++ ttHtautauAnalyzer/test/submitCrabFastSim2016.sh
@@ -3,10 +3,10 @@
 samplelist='../data/SampleList_FastSim2016.txt'
 
 # 2lss1tau
-submitCrabJobs.py $samplelist 2lss1tau loose --channel_list channels_fastsim16.txt --prefix fs16_ --dbs phys03 -d
+submitCrabJobs.py $samplelist 2lss1tau loose --channel_list channels_fastsim16.txt --prefix fs16_ --dbs phys03 -a -b -d
 
 # 1l2tau
-submitCrabJobs.py $samplelist 1l2tau loose --channel_list channels_fastsim16.txt --prefix fs16_ --dbs phys03 -d
+submitCrabJobs.py $samplelist 1l2tau loose --channel_list channels_fastsim16.txt --prefix fs16_ --dbs phys03 -a -b -d
 
 # 3l1tau
-#submitCrabJobs.py $samplelist 3l1tau loose --channel_list channels_fastsim16.txt --prefix fs16_ --dbs phys03 -d
+submitCrabJobs.py $samplelist 3l1tau loose --channel_list channels_fastsim16.txt --prefix fs16_ --dbs phys03 -a -b -d
