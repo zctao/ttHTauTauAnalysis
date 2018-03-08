391d934f24936ae61fad673e992ea41c69b76f5d
zctao <zt75@cornell.edu>
2018-03-08 00:03:31 -0600

    Split less for FastSim crab jobs

--- ttHtautauAnalyzer/test/submitCrabFastSim2016.sh
+++ ttHtautauAnalyzer/test/submitCrabFastSim2016.sh
@@ -2,11 +2,11 @@
 
 samplelist='../data/SampleList_FastSim2016.txt'
 
-# 2lss1tau
-submitCrabJobs.py $samplelist 2lss1tau loose --channel_list channels_fastsim16.txt --prefix fs16_ --dbs phys03 -a -b -d
-
 # 1l2tau
-submitCrabJobs.py $samplelist 1l2tau loose --channel_list channels_fastsim16.txt --prefix fs16_ --dbs phys03 -a -b -d
+submitCrabJobs.py $samplelist 1l2tau loose --channel_list channels_fastsim16.txt --prefix fs16_ --dbs phys03 -b -d
+
+# 2lss1tau
+submitCrabJobs.py $samplelist 2lss1tau loose --channel_list channels_fastsim16.txt --prefix fs16_ --dbs phys03 -b -d
 
 # 3l1tau
-submitCrabJobs.py $samplelist 3l1tau loose --channel_list channels_fastsim16.txt --prefix fs16_ --dbs phys03 -a -b -d
+submitCrabJobs.py $samplelist 3l1tau loose --channel_list channels_fastsim16.txt --prefix fs16_ --dbs phys03 -b -d
