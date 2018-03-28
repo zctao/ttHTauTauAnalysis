6ceaab8a8510ad862acc9a13d5ca6ccf4745f2f3
zctao <zt75@cornell.edu>
2018-03-28 15:50:50 -0500

    Update hlt paths

--- ttHtautauAnalyzer/src/TriggerHelper.cc
+++ ttHtautauAnalyzer/src/TriggerHelper.cc
@@ -15,7 +15,7 @@ const std::vector<std::string> TriggerHelper::hlt_paths_2l_ = {
 	"HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
 	"HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_v",
 	// di mu
-	"HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v",
+	//"HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v",
 	"HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v",
 	"HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8_v4",
 	// e mu
@@ -31,9 +31,9 @@ const std::vector<std::string> TriggerHelper::hlt_paths_ltau_ = {
 
 const std::vector<std::string> TriggerHelper::hlt_paths_3l_ = {
 	"HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdL_v",
-	"HLT_TripleMu_12_10_5_v",
 	"HLT_Mu8_DiEle12_CaloIdL_TrackIdL_v",
-	"HLT_DiMu9_Ele9_CaloIdL_TrackIdL_DZ_v"
+	"HLT_DiMu9_Ele9_CaloIdL_TrackIdL_DZ_v",
+	"HLT_TripleMu_12_10_5_v"
 };
 
 // Filters
