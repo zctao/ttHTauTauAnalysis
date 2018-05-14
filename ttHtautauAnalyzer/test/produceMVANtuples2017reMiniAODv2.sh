5a71efb0aa81efd2267b49ad249f475da9faf3c2
zctao <zt75@cornell.edu>
2018-05-14 09:57:44 -0500

    Update script to produce mva ntuples

--- ttHtautauAnalyzer/test/produceMVANtuples2017reMiniAODv2.sh
+++ ttHtautauAnalyzer/test/produceMVANtuples2017reMiniAODv2.sh
@@ -7,6 +7,7 @@ outdir='/uscms/home/ztao/nobackup/mvaNtuples/'
 version='may2018'
 
 samples='ttHJetToNonbb data_e'
+#samples='ttHJetToNonbb TTW TTW_psw TTZ TTZ_M1to10 TTGJets WZ ZZ ZZ_ext WW WWW WWZ WZZ ZZZ TTWW TTTT tZq WWds TTToDiLep TTToDiLep_psw TTToSemiLep TTToSemiLep_psw TTToHad TTToHad_psw ST_sLep ST_sLep_psw ST_tT ST_tTbar ST_tWT ST_tWT_psw ST_tWTbar ST_tWTbar_psw DYJets_M50 DYJets_M50_ext DYJets_M10to50 data_e data_mu data_dieg data_dimu data_mueg'
 #corrections='jesup jesdown tesup tesdown'
 
 mkdir -p $outdir$version
