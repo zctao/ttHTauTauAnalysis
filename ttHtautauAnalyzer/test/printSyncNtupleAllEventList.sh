e5921b4c22383bfca9368c650f4689c7ccb5df0a
zctao <zt75@cornell.edu>
2018-05-30 12:02:47 -0500

    Update script to print sync ntuple events

--- ttHtautauAnalyzer/test/printSyncNtupleAllEventList.sh
+++ ttHtautauAnalyzer/test/printSyncNtupleAllEventList.sh
@@ -48,3 +48,29 @@ printEventList.sh $indir$ntuple $outidr'evlist_2l2tau_fake.dat' syncTree_2l2tau_
 RootScanParser.sh $outidr'evlist_2l2tau_fake.dat' evlist_2l2tau_fake.txt
 rm $outidr'evlist_2l2tau_fake.dat'
 mv evlist_2l2tau_fake.txt $outdir
+
+printEventList.sh $indir$ntuple $outidr'evlist_ttWctrl_sr.dat' syncTree_ttWctrl_SR
+RootScanParser.sh $outidr'evlist_ttWctrl_sr.dat' evlist_ttWctrl_sr.txt
+rm $outidr'evlist_ttWctrl_sr.dat'
+mv evlist_ttWctrl_sr.txt $outdir
+
+printEventList.sh $indir$ntuple $outidr'evlist_ttWctrl_fake.dat' syncTree_ttWctrl_Fake
+RootScanParser.sh $outidr'evlist_ttWctrl_fake.dat' evlist_ttWctrl_fake.txt
+rm $outidr'evlist_ttWctrl_fake.dat'
+mv evlist_ttWctrl_fake.txt $outdir
+
+printEventList.sh $indir$ntuple $outidr'evlist_ttWctrl_flip.dat' syncTree_ttWctrl_Flip
+RootScanParser.sh $outidr'evlist_ttWctrl_flip.dat' evlist_ttWctrl_flip.txt
+rm $outidr'evlist_ttWctrl_flip.dat'
+mv evlist_ttWctrl_flip.txt $outdir
+
+printEventList.sh $indir$ntuple $outidr'evlist_ttZctrl_sr.dat' syncTree_ttZctrl_SR
+RootScanParser.sh $outidr'evlist_ttZctrl_sr.dat' evlist_ttZctrl_sr.txt
+rm $outidr'evlist_ttZctrl_sr.dat'
+mv evlist_ttZctrl_sr.txt $outdir
+
+printEventList.sh $indir$ntuple $outidr'evlist_ttZctrl_fake.dat' syncTree_ttZctrl_Fake
+RootScanParser.sh $outidr'evlist_ttZctrl_fake.dat' evlist_ttZctrl_fake.txt
+rm $outidr'evlist_ttZctrl_fake.dat'
+mv evlist_ttZctrl_fake.txt $outdir
+
