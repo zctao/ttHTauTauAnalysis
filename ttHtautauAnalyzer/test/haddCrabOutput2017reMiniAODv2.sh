5575f1d5fd33172a5d0175f34fec5b685bdc4e8d
zctao <zt75@cornell.edu>
2018-05-29 21:50:20 -0500

    Update haddCrabOutput2017reMiniAODv2.sh

--- ttHtautauAnalyzer/test/haddCrabOutput2017reMiniAODv2.sh
+++ ttHtautauAnalyzer/test/haddCrabOutput2017reMiniAODv2.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
+# sample list
+samplelist=${1:-'../dataFiles/DatasetList_2017reMiniAODv2.csv'}
+#samplelist='../dataFiles/DatasetList_2017reMiniAODv2.csv'
+
 # my eos directory
 eosrootdir='/store/user/ztao/ttHtaus_94X/'
 
-# sample list
-samplelist='../dataFiles/DatasetList_2017reMiniAODv2.csv'
-
 # version
-version='may2018'
+version='may2018v2'
 
 # crab job prefix
-#prefix='crab_2018may_'
-prefix='crab_2018may'
+prefix='crab_2018mayv2_'
 
 # timestamp
 timestamp=`date +%Y%m%d`
 #echo $timestamp
-nlist=ntuplelist_2017remaodv2_$timestamp.log
+nlist=ntuplelist_2017remaodv2_$version_$timestamp.log
 touch $nlist
 
 ###############
@@ -24,15 +24,15 @@ touch $nlist
 analysis='incl'
 
 #MC
-samples='ttHJetToNonbb TTW TTW_psw TTZ TTZ_psw WZ TTToDiLep TTToDiLep_psw TTToSemiLep TTToSemiLep_psw TTToHad TTToHad_psw'
-# TTGJets TGJets WG ZG ZZ WW WWds WpWp WZZ WWZ WWW ZZZ tZq TTTT'
+samples=''
+#samples='ttHJetToNonbb TTW TTW_psw TTZ TTZ_psw WZ WZTo3LNu TTGJets TTZ_M1to10 ZZ ZZ_ext WW WWTo2L2Nuds WZZ WWZ WWW ZZZ tZq TTTT TTWW ggHZZ4l WWds' #
+#samples='ttHToNonbb TTToDiLep TTToDiLep_psw TTToSemiLep TTToSemiLep_psw TTToHad TTToHad_ps DYJets_M50 DYJets_M50_ext DYJets_M10to50 ST_sLep ST_sLep ST_tT ST_tTbar ST_tWT ST_tWT ST_tWTbar ST_tWTbar'
+# TGJets WG ZG WJets
+# ST_tWll WpWpJJ VHToNonbb tHW tHq
+
 #corrections='jesup jesdown tesup tesdown'
 corrections=''
 skipNominal=false
-
-# Data
-datasets='mu e dimu dieg mueg'
-dataset_era='2017b 2017c 2017d 2017e 2017f'
 
 # MC
 for sample in $samples
@@ -51,17 +51,37 @@ do
 	echo 'hadd sample' $sample 'takes' $((end-start))'s'
 done
 
-# Data	
-for dataset in $datasets
-do
-	collection=''
-	for era in $dataset_era
-	do
-		collection=$collection'data_'$dataset'_'$era' '
-	done
-	
-	start=`date +%s`
-	haddCrabOutputs.sh $analysis $samplelist $eosrootdir $version $prefix'_' $nlist ntuple_'data_'$analysis $collection
-	end=`date +%s`
-	echo 'hadd' $dataset 'dataset takes' $((end-start))'s'
-done
+# Data
+datasets=''
+#datasets='mu e dimu dieg mueg'
+dataset_era=''
+#dataset_era='2017b 2017c 2017d 2017e 2017f'
+
+#for dataset in $datasets
+#do
+#	collection=''
+#	for era in $dataset_era
+#	do
+#		collection=$collection'data_'$dataset'_'$era' '
+#	done
+#	
+#	start=`date +%s`
+#	haddCrabOutputs.sh $analysis $samplelist $eosrootdir $version $prefix $nlist ntuple_'data_'$analysis $collection
+#	end=`date +%s`
+#	echo 'hadd' $dataset 'dataset takes' $((end-start))'s'
+#done
+
+collection_e='data_e_2017b data_e_2017c data_e_2017c_missingLumis data_e_2017d data_e_2017e data_e_2017f'
+#haddCrabOutputs.sh $analysis $samplelist $eosrootdir $version $prefix $nlist ntuple_'data_'$analysis $collection_e
+
+collection_mu='data_mu_2017b data_mu_2017c data_mu_2017d data_mu_2017d_missingLumis data_mu_2017e data_mu_2017f data_mu_2017f_missingLumis'
+#haddCrabOutputs.sh $analysis $samplelist $eosrootdir $version $prefix $nlist ntuple_'data_'$analysis $collection_mu
+
+collection_mueg='data_mueg_2017b data_mueg_2017c data_mueg_2017c_missingLumis data_mueg_2017d data_mueg_2017e data_mueg_2017f'
+#haddCrabOutputs.sh $analysis $samplelist $eosrootdir $version $prefix $nlist ntuple_'data_'$analysis $collection_mueg
+
+collection_dimu='data_dimu_2017b data_dimu_2017c data_dimu_2017d data_dimu_2017e data_dimu_2017e_missingLumis data_dimu_2017f'
+#haddCrabOutputs.sh $analysis $samplelist $eosrootdir $version $prefix $nlist ntuple_'data_'$analysis $collection_dimu
+
+collection_dieg='data_dieg_2017b data_dieg_2017c data_dieg_2017d data_dieg_2017e data_dieg_2017f data_dieg_2017f_missingLumis'
+#haddCrabOutputs.sh $analysis $samplelist $eosrootdir $version $prefix $nlist ntuple_'data_'$analysis $collection_dieg
